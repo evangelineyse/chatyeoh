09c84d845d7a2be697743e8fd1dc7b7499d08311
Evangeline <evangeline@192.168.1.9>
2026-02-12 22:25:37 +0800

    Add HEAD handler

--- bot.py
+++ bot.py
@@ -160,6 +160,10 @@ class Handler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b"Bot is running")
 
+        def do_HEAD(self):
+        self.send_response(200)
+        self.end_headers()
+
 
 def run_dummy_server():
     port = int(os.environ.get("PORT", 10000))
